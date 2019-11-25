d505952ee682e0ca8047831837ae9bb4d95b274d
Your Name <jack.sullivan96@gmail.com>
2019-11-25 14:59:34 -0500

    pure workers

--- Workers/Splitter.js
+++ Workers/Splitter.js
@@ -3,7 +3,7 @@ const resolve = require('path').resolve
 const final = resolve('./FinalText.txt')
 
 var envelope = {
-    pieces: []
+    pieces: [{}]
 }
 
 
@@ -27,43 +27,58 @@ fs.readFile(final, 'utf8', function (err, data) {
             str += data[i]
         }
     }
-    console.log(prePiece)
+
+    console.log(prePiece[1])
+    envelope.pieces[0].title = "Intro"
+    envelope.pieces[0].text = prePiece[0]
+    //need link from first slice
 
     if (prePiece.length > 0) {
         str = ""
         //title thing
-        for (var j = 0; j < prePiece; j++) {
+        for (var j = 0; j < prePiece.length; j++) {           
             // need break right after 
             // different "for" loops (doesn't change)
             /* 
             
             */
+            const miniObj = {}
             const item = prePiece[j]
             
             //Titles
             for (var k = 0; k < item.length; k++) {
-                var indHold = []
-                if (item[k] == "*") {
+                if (item[k] == "&") {
                     // indHold.push(k)
-                    // 
+                    str+=item[k]
                 }
+                else if (prePiece[j].search("\r\n\r\n") === k ) {
+                    // want to add to string all at once, then skip to end of line
+                    // -> just splice it in, and keep going. TODO: trim returns off end of beginning of each text item later
+                    console.log(k)
+                    
+                    if (j < 1) {
+                        miniObj.title = str
+                        // console.log("STRING", str)
+                        str = ""
+                    }
+                    
 
-                //problem here: hit all line breaks; TODO: Hit only linebreaks after push
 
-                else if (prePiece[j] == "\r\n\r\n" & str.toLowerCase().indexOf("\r\n\r\n") === -1) {
-                    indHold.push(k)
-                    str = ""
-                    continue;
+
+
                 } else {
                     str += prePiece[j]
                 }
             }
 
+            envelope.pieces.push(miniObj)
+
             //append now to pieces ( beginning pieces' objects)
+
+        
         }
 
     }
-    console.log(prePiece)
 
 
 
