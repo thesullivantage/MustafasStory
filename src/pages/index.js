f8ea3577c0ecd260493490b18dab86ba1a1a986c
Your Name <jack.sullivan96@gmail.com>
2019-11-29 02:10:17 -0500

    react pseudocode

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,37 @@ import React, { Component } from "react";
 import "./styling/index.css"
 
 class Index extends Component {
+    // import json file
+    // Declare initial state of object as the intro object
+        // no need to iterate from there, only use links
+    // use alias object again to more quickly access pieces
+    //handle:
+        // link
+            // simple search for index, then change state to correct index of result object (obj imported)
+                
+            // start text on first index of 
+        // return 
+            // same as link, search for index, and change state to found index
+        // "paginate"
+            // only go forward
+            // conditionals: 
+                // if current obj has return ( render that in place of next button)
+                    // TODO: next button has cond. rendering that hides/ shows arrow or back graphic, need sub component
+                // else if *not (go forward until you get to the last when next clicked, then go to prompt
+                        // setState: objInd + 1 (if not the end)
+                            // cond. rendering will take care of the rest 
+                // )
+                
+                
+
+    // cond renedering for prompt/no prompt
+
+    // ANIMATIONS/GRAPHIC STUFF:
+        // TODO: Use css to hide or other method?
+        // easy mobile responsiveness now 
+        // Button hover effect: easy css
+        // ditch 2 div type of thing
+
 
     constructor(props) {
         super(props)
@@ -10,17 +41,25 @@ class Index extends Component {
         };
     }
 
-    // state = {
-    //     userQr: ""
-    // };
+    state = {
+        // no objects needed here
+        objInd: 0,
+        textInd: 0,
+    };
 
 
     componentDidMount = () => {
-        this.setState({
-            userQr: "hello"
-        });
+
+        
+        // this.setState({
+        //     triggerObj: ""
+        // });
     }
 
+    handleLink = () => {
+        // change state of triggerObj 
+    }
+ 
     render() {
 
         return (
